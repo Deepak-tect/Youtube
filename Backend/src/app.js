f62598ec28ab6086d3af2a80ad3b1701dffe6f89
deepak-tect <dddeepak888@gmail.com>
2023-12-26 10:29:11 +0530

    initial setup done

---
+++ Backend/src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+const app = express();
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}))
+
+app.use(express.json({limit:"16kb"}))
+app.use(express.urlencoded({extended:true ,limit:"16kb"})) // url handle spaces as %20 or sometime + , to understand this we use this line
+app.use(express.static("public")) // to store temporary data duch as file , img into public folder
+app.use(cookieParser()); // perform CRUD operation in cookie 
---
+++ Backend/src/index.js
@@ -0,0 +1,16 @@
+import connectDB from "./db/index.js";
+import express from "express";
+
+const app = express();
+import dotenv from "dotenv"
+dotenv.config({
+    path:'./env'
+})
+
+connectDB().then(()=>{
+    app.on("error",(error)=>{
+        console.log("error",error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 8000 , ()=>console.log(`Server running on ${process.env.PORT}`))
+}).catch((error)=>console.log("MongoDB connection failed",error))
